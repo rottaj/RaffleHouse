05148f20b7929b73d7efee4a996879b759468052
rottaj <jackrotta12@gmail.com>
2022-01-11 01:09:48 -0500

    fixed.. ready to push to database

--- populate_database.js
+++ populate_database.js
@@ -24,13 +24,28 @@ async function getTokens(contract) {
   const totalAmount = await contract.totalSupply();
   return totalAmount
 }
+
+async function getMetaData(contract, totalSupply) {
+  for (let i=1; i<=totalSupply-1; i++ ) {
+  console.log("TOKEN ID ", i)
+  try {
+    let metaData = await contract.tokenURI(i);
+      console.log(metaData);
+    } catch (error) {
+      console.log(error);
+    }
+  } 
+}
+
 //var contract = getContract();
 var contract = new ethers.Contract(address, _abi, provider);
 console.log(contract);
 var totalAmount = getTokens(contract).then(res => {
   console.log(parseInt(res, 16))
+  getMetaData(contract, parseInt(res, 16)); 
 });
 console.log(totalAmount);
+
 /*
 pool.query('SELECT * FROM Eyescream', (err, res) => {
   console.log(err, res);
