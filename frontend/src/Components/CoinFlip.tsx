da7a95ce72dc1b80a5f8898ee36ab07136591b7c
rottaj <jackrotta12@gmail.com>
2022-01-21 17:31:58 -0500

    Added some css shitacki

--- frontend/src/Components/CoinFlip.tsx
+++ frontend/src/Components/CoinFlip.tsx
@@ -9,9 +9,9 @@ export default class CoinFlip extends React.Component <Props>{
     render() {
         return (
             <div className="CoinFlip-Div-Main">
-                <h6> Creator: {this.props.coinFlip.creatorAddress}</h6>
-                <h6> Winner: {this.props.coinFlip.winner}</h6>
-                <h6>Buy In Price: {this.props.coinFlip.buyInPrice} eth </h6>
+                <h6 className="CoinFlip-Creator-h6"> Creator: {this.props.coinFlip.creatorAddress}</h6>
+                <h6 className="CoinFlip-Winner-h6"> Winner: {this.props.coinFlip.winner}</h6>
+                <h6 className="CoinFlip-BuyIn-h6">Buy In Price: {this.props.coinFlip.buyInPrice} eth </h6>
             </div>
         )
     }
