494cf4070fd85de828160bbb6aed49e82cd1dddf
rottaj <jackrotta12@gmail.com>
2022-02-09 03:28:05 -0500

    Refactored RaffleViewer

--- frontend/src/Containers/RaffleViewer.tsx
+++ frontend/src/Containers/RaffleViewer.tsx
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 import { _abi } from "../interfaces/Eyescream_Interface";
 import { _Raffle_abi } from '../interfaces/RaffleEscrow_Interface';
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import MenuItems from '../Components/MenuItems';
 import Deposit from "../Components/Deposit";
 import PlayersList from "../Components/PlayersList";
@@ -21,21 +21,19 @@ const ETHERSCAN_API_KEY = 'JPARDRW9CAVF9ZKISWVC3YYM6RP93JNQUC';
 
 
 declare let window: any;
-export default class RaffleViewer extends React.Component {
+const RaffleViewer = () => {
 
 
-    state: any = {
-        tokenMetaData: {},
-        isDepositOpen: false,
-        raffleContractAddress: "",
-        raffleBalance: 0,
-        players: [],
-        gameInfo: [],
-        account: ""
-    }
+    const [tokenMetaData, setTokenMetaData]: any = useState({});
+    const [isDepositOpen, setIsDepositOpen]: any = useState(false);
+    const [raffleContractAddress, setRaffleContractAddress]: any = useState('');
+    const [raffleBalance, setRaffleBalance]: any = useState('');
+    const [players, setPlayers]: any = useState([]);
+    const [gameInfo, setGameInfo]: any = useState([]);
+    const [account, setAccount]: any = useState('');
 
 
-    fetchNFTs = async (contractAddress: any) => {
+    const fetchNFTs = async (contractAddress: any) => {
         if (window.ethereum) {
             var provider = new ethers.providers.Web3Provider(window.ethereum);
             // const signer = provider.getSigner();
@@ -51,13 +49,13 @@ export default class RaffleViewer extends React.Component {
                 return res.json();
             })
             .then(data => {
-                this.getMetaData(data.result[0]) // POTENTIAL FIX IS PROBALY GOOD
+                getMetaData(data.result[0]) // POTENTIAL FIX IS PROBALY GOOD
             })
 
         }
     }
 
-    getMetaData = async (token: any) => {
+    const getMetaData = async (token: any) => {
         console.log(token)
         if (window.ethereum) {
             var provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -73,9 +71,7 @@ export default class RaffleViewer extends React.Component {
                     let metaData = await contract.tokenURI(parseInt(token.tokenID))
                     fetch(metaData).then(res => {return res.json()}).then(data => {
                         token['image'] = data.image
-                        this.setState({
-                            tokenMetaData: token
-                        })
+                        setTokenMetaData(token)
                     })
                 }
             }
@@ -86,96 +82,90 @@ export default class RaffleViewer extends React.Component {
         }
     }
 
-    getUniqueAddresses(array: any) {
+    const getUniqueAddresses = (array: any) => {
         let unique = array.filter((item: any, i: any, ar: any) => ar.indexOf(item) === i);
         return unique;
     }
 
-    getOccurances(array: any, val: any) {
+    const getOccurances = (array: any, val: any) => {
         let ticketNumber = array.reduce((a: any, v: any) => (v === val ? a + 1 : a), 0);
         return ticketNumber;
     }
 
-    getTickets = async (uniqueAddresses: any, tickets: any) => {
+    const getTickets = async (uniqueAddresses: any, tickets: any) => {
         console.log(tickets)
         for (let i=0; i<=uniqueAddresses.length; i++ ) {
-            let ticketNumber = this.getOccurances(tickets, uniqueAddresses[i])
+            let ticketNumber = getOccurances(tickets, uniqueAddresses[i])
             if (uniqueAddresses[i] !== undefined) {
                 console.log("TESTING GET_TICKETS", uniqueAddresses[i], ticketNumber, (ticketNumber * 0.01).toFixed(2))
                 let player = {address: uniqueAddresses[i], tickets: ticketNumber, totalEth: (ticketNumber * 0.01).toFixed(2), chance: ((ticketNumber / tickets.length) * 100).toFixed(2)}
-                this.setState({
-                    players: [...this.state.players, player]
-                })
+                setPlayers((players:any) => [...players, player])
             }
         }
 
     }
 
-    handleDepositClicked = () => {
-        this.setState({
-            isDepositOpen: !this.state.isDepositOpen
-        })
+    const handleDepositClicked = () => {
+        setIsDepositOpen(!isDepositOpen)
     }
 
 
-    async componentDidMount() {
+    useEffect(() => {
         const contractAddress = window.location.pathname.split('/').at(-1);
-        this.setState({
-            raffleContractAddress: contractAddress
-        })
-        console.log(contractAddress);
-        this.fetchNFTs(contractAddress); // refactor (can do this later)
+        setRaffleContractAddress(contractAddress);
 
-        var provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        let contract = new ethers.Contract(contractAddress, _Raffle_abi, signer);
-        const gameInfo = await contract.getGameInfo()
-        this.setState({
-            gameInfo: gameInfo
-        })
-        const tickets = await contract.getTickets();
-        const uniqueAddresses = this.getUniqueAddresses(tickets);
-        this.getTickets(uniqueAddresses, tickets);
-        if(window.ethereum) {
+        const mountRaffleGameInfo = async() => {
             var accounts = await window.ethereum.send('eth_requestAccounts');
             const account = accounts.result[0];
-            this.setState({account: account});
+            setAccount(account);
+            var provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            let contract = new ethers.Contract(contractAddress, _Raffle_abi, signer);
+            const gameInfo = await contract.getGameInfo()
+            fetchNFTs(contractAddress); // refactor (can do this later)
+
+            setGameInfo(gameInfo);
+            const tickets = await contract.getTickets();
+            const uniqueAddresses = getUniqueAddresses(tickets);
+            getTickets(uniqueAddresses, tickets);
         }
-    }
+        if(window.ethereum) {
+            mountRaffleGameInfo();
+        }
+    }, [])
 
 
-    render() {
-        return (
-            <div className="RaffleViewer-Div-Main">
-                {/* <Header/> */}
-                <MenuItems account={this.state.account}/>
-                <Messages/>
-                <div className="WinnerBox-Main-Div">
-                <h3 className="Title-Raffle-Winner">Raffle Winner:</h3>
-                {this.state.gameInfo.winner !== "0x0000000000000000000000000000000000000000" ?
-                    <h3>{this.state.gameInfo.winner}</h3>
-                :
-                    <h3>Winner not picked</h3>
-                }
-                { /*    // Will add later
-                <h3 className="Title-Raffle-Total-Pot">Total</h3> 
-                <h3 className="Title-Raffle-Chance">Chance</h3>
-                */ }
-                </div>
-                {console.log("GAME INNFO", this.state.gameInfo)}
-                <div className="RaffleViewer-Viewer-Container">
-                    <div className="Token-Image-Div">
-                        <img className="Token-Image"src={this.state.tokenMetaData.image}></img>
-                        <Button onClick={this.handleDepositClicked} variant="contained" type="submit" style={{maxHeight: '55px'}}>
-                            Deposit
-                        </Button>
-                        <Deposit tokenMetaData={this.state.tokenMetaData} isDepositOpen={this.state.isDepositOpen} raffleContractAddress={this.state.raffleContractAddress}/>
-                    </div>
-
-                    <PlayersList players={this.state.players}/>
-                </div>
-                <Footer/>
+    return (
+        <div className="RaffleViewer-Div-Main">
+            {/* <Header/> */}
+            <MenuItems account={account}/>
+            <Messages/>
+            <div className="WinnerBox-Main-Div">
+            <h3 className="Title-Raffle-Winner">Raffle Winner:</h3>
+            {gameInfo.winner !== "0x0000000000000000000000000000000000000000" ?
+                <h3>{gameInfo.winner}</h3>
+            :
+                <h3>Winner not picked</h3>
+            }
+            { /*    // Will add later
+            <h3 className="Title-Raffle-Total-Pot">Total</h3> 
+            <h3 className="Title-Raffle-Chance">Chance</h3>
+            */ }
             </div>
-        )
-    }
+            <div className="RaffleViewer-Viewer-Container">
+                <div className="Token-Image-Div">
+                    <img className="Token-Image"src={tokenMetaData.image}></img>
+                    <Button onClick={handleDepositClicked} variant="contained" type="submit" style={{maxHeight: '55px'}}>
+                        Deposit
+                    </Button>
+                    <Deposit tokenMetaData={tokenMetaData} isDepositOpen={isDepositOpen} raffleContractAddress={raffleContractAddress}/>
+                </div>
+
+                <PlayersList players={players}/>
+            </div>
+            <Footer/>
+        </div>
+    )
 }
+
+export default RaffleViewer
