a37d14879748b365fd1ed4cc9455c3e647813da9
blakesenn <blakesenn1@yahoo.com>
2022-02-16 19:19:37 -0600

    Sidebar shit

--- frontend/src/Components/Messages.tsx
+++ frontend/src/Components/Messages.tsx
@@ -4,18 +4,21 @@ import {
   MessagesAddress,
   _Messages_abi,
 } from "../interfaces/Messages_interface";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Button, Input, Skeleton, Stack, Text } from "@chakra-ui/react";
 import { useContext } from "react";
 import { MetaMaskUserContext } from "../utils/contexts";
 
 declare let window: any;
 const Messages = () => {
   const [messages, setMessages]: any = useState([]);
+  const [isMessagesLoading, setIsMessagesLoading] = useState(true);
+  const [message, setMessage] = useState("");
 
   const { user: myAddress } = useContext(MetaMaskUserContext);
 
   const getMessages = async () => {
     if (window.ethereum) {
+      const tempMessages = [];
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const messagesContract = await new ethers.Contract(
@@ -29,8 +32,10 @@ const Messages = () => {
         var messageInstance = await messagesContract.getMessageByIndex(i);
         message["messager"] = messageInstance["messager"];
         message["message"] = messageInstance["message"];
-        setMessages((messages: any) => [...messages, message]);
+        tempMessages.push(message);
       }
+      setMessages(tempMessages);
+      setIsMessagesLoading(false);
     }
   };
 
@@ -41,74 +46,86 @@ const Messages = () => {
   }, []);
 
   const handleSubmit = async (e: any) => {
-    e.preventDefault();
     var provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const messagesContract = await new ethers.Contract(
+    const messagesContract = new ethers.Contract(
       MessagesAddress,
       _Messages_abi,
       signer
     ); // connect to Raffles Contract
-    const messageTxn = messagesContract.createMessage(e.target[0].value);
+    await messagesContract.createMessage(message);
   };
 
   return (
-    <Box
-      backgroundColor="#0d0b0"
-      zIndex="9000"
-      textAlign="center"
-      position="fixed"
-      width="15%"
-      float="right"
-      color="white"
-      right="10px"
-      fontFamily="Arial, Helvetica, sans-serif"
-      height="100%"
-      overflowY="scroll"
-      borderRadius="30px"
-      opacity="83%"
-    >
-      <Heading fontSize="lg">Messages </Heading>
-      <Box>
-        <Box>
-          {messages.map((message: any, index: number) => {
+    <Box color="black" fontFamily="Arial, Helvetica, sans-serif" height="100%">
+      <Box px="22px" pb="40px">
+        {isMessagesLoading ? (
+          <Stack>
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+            <Skeleton height="21px" />
+          </Stack>
+        ) : (
+          messages.map((message: any, index: number) => {
             return (
               <Box key={index}>
-                <Heading fontSize="sm">
-                  {message.messager === myAddress[myAddress.length - 1]
+                <Text fontWeight="bold" fontSize="sm">
+                  {message.messager.toLowerCase() === myAddress
                     ? "You" + ": "
                     : message.messager.substring(0, 6) +
                       "..." +
                       message.messager.substring(36, 40) +
                       ": "}
-                </Heading>
-                <Heading fontSize="sm">{message.message}</Heading>
+                </Text>
+                <Text fontSize="sm">{message.message}</Text>
               </Box>
             );
-          })}
-          <Box height="100%" minHeight="200px">
-            <form
-              className="CreateMessage-Form"
-              onSubmit={(e) => handleSubmit(e)}
-            >
-              {/* <TextField
-                className="Messages-Create-New-Message"
-                sx={{
-                  background: "white",
-                  color: "black",
-                  input: { color: "black" },
-                }}
-                placeholder="Message"
-                id="filled-basic"
-                label="Write something"
-                variant="filled"
-              ></TextField> */}
-              {/* <Button variant="contained" type="submit" style={{maxHeight: '55px'}}>
-                        Send Message
-                    </Button> */}
-            </form>
-          </Box>
-        </Box>
+          })
+        )}
+      </Box>
+      <Box pos="fixed" bottom={0} m={0} p={0}>
+        <Input
+          sx={{
+            background: "white",
+            color: "black",
+            input: { color: "black" },
+          }}
+          placeholder="Send message"
+          id="filled-basic"
+          onChange={(e) => setMessage(e.target.value)}
+        ></Input>
+        <Button
+          isFullWidth
+          onClick={() => handleSubmit(message)}
+          style={{ maxHeight: "55px" }}
+        >
+          Send Message
+        </Button>
+        {/* </form> */}
       </Box>
     </Box>
   );
