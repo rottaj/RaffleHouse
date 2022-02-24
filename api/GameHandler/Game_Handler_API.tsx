231e3a6a4b6aa3f86884d3618c30f87c9b2495f9
rottaj <jackrotta12@gmail.com>
2022-02-24 15:00:16 -0500

    Modified GameHandler.. client side gcloud will work now

--- api/GameHandler/Game_Handler_API.tsx
+++ api/GameHandler/Game_Handler_API.tsx
@@ -1,23 +1,31 @@
 const ethers = require('ethers');
 var bodyParser = require('body-parser')
 var app = require('express')();
+const cors = require('cors');
 var http = require('http').createServer(app);
 const LinkInterface = require('../interfaces/ChainLink_Interface')
+require('dotenv').config();
+app.use(cors())
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
-
-const provider:any = new ethers.providers.JsonRpcProvider(process.env.RINKEBY_URL);
+const provider = new ethers.providers.JsonRpcProvider(process.env.RINKEBY_URL);
 const signer = new ethers.Wallet(process.env.SERVER_PHRASE, provider);
 const abi = JSON.parse(LinkInterface._abi_two.result);
 const ChainLinkContract = new ethers.Contract(LinkInterface.linkAddress, abi, signer); // add this later ?
 
-
-app.get('/fundCoinFlipGame', async (req, res) => {
-    const chainLinkTxn_HighRollers = await ChainLinkContract.transfer(req.body.contractAddress, ethers.utils.parseUnits("1"));
-    console.log(chainLinkTxn_HighRollers)
+app.post('/fundGame', async (req, res) => {
+    console.log(req.body)
+    if (req.body.contractAddress) {
+      const chainLinkTxn_FundGame = await ChainLinkContract.transfer(req.body.contractAddress, ethers.utils.parseUnits("0.1"));
+      console.log(chainLinkTxn_FundGame)
+    }
 })
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
+module.exports = app;
 
