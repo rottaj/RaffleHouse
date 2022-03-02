7db823ca57a54dc7e9f83b4ece8117eaf58a6035
rottaj <jackrotta12@gmail.com>
2022-03-01 23:42:19 -0500

    Added History, built CoinFlips component

--- frontend/src/Containers/MyHistory.tsx
+++ frontend/src/Containers/MyHistory.tsx
@@ -1,17 +1,65 @@
-import BaseContainer from "./BaseContainers/BaseContainer"
+import { useState, useEffect, useContext } from 'react';
+import BaseContainer from "./BaseContainers/BaseContainer";
+import CoinFlip from "../Components/CoinFlip";
 import {
     Box,
+    Grid,
+    GridItem,
     Heading
 } from "@chakra-ui/react";
-
+import { db } from "../firebase-config";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  increment,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { MetaMaskUserContext } from "../utils/contexts";
 
 const MyHistory = () => {
+    const [coinFlips, setCoinFlips]: any = useState([]);
+    const coinflipsCollectionRef = collection(db, "coinflips");
+    const { user } = useContext(MetaMaskUserContext);
+    
+    useEffect(() => {
+        const getGameData = async () => {
+            console.log("USER", user)
+            const coinFlipsQuery = query(coinflipsCollectionRef, where("creatorAddress", "==", user), where("joineeAddress", "==", user));
+            console.log("Query", coinFlipsQuery)
+            const coinFlipsData = await getDocs(coinFlipsQuery);
+            console.log("COINDATAFO", coinFlipsData)
+            coinFlipsData.docs.map((doc) => {
+                console.log("DATA", doc)
+                setCoinFlips((coinFlips) => [...coinFlips, doc.data()])
+            })
+        }
+
+        getGameData();
+    }, [])
+
     return (
         <BaseContainer>
             <Box textAlign="center" color="white">
                 <Heading> Game History </Heading>
-                <Box>
+                <Box width="50%" ml="25%">
                     <Heading>CoinFlips</Heading>
+                    <Grid 
+                        color="white"
+                        templateColumns='repeat(2, 1fr)' 
+                        gap={2}
+                    >
+                        {console.log("COIN FLIPS", coinFlips)}
+                        {coinFlips.map((coinFlip: any) => {
+                            console.log("COINFLIP", coinFlip)
+                            return (<GridItem><CoinFlip coinFlip={coinFlip}></CoinFlip></GridItem>)
+                        })}
+                    </Grid>
                 </Box>
                 <Box>
                     <Heading>Raffles</Heading>
